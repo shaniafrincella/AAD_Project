8a874c4a9b6b5397cd5d68ea05dadf79e74e698b
Shania <shaniafrincella@gmail.com>
2020-06-03 03:23:19 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/aadproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.aadproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
