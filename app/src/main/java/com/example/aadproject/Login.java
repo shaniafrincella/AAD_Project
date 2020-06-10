5e8b6c50270d5f60484787c6829c5698ec9abd77
Shania <shaniafrincella@gmail.com>
2020-06-11 00:56:55 +0800

    Create Splash layout, update login and register layout; and modify AndroidManifest.

--- app/src/main/java/com/example/aadproject/Login.java
+++ app/src/main/java/com/example/aadproject/Login.java
@@ -2,7 +2,9 @@ package com.example.aadproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onCreateAccountClick(View view) {
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/example/aadproject/Register.java
+++ app/src/main/java/com/example/aadproject/Register.java
@@ -2,13 +2,43 @@ package com.example.aadproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
+    private EditText editTextFullName, editTextEmail, editTextPassword;
+    private Button buttonRegister, buttonLogin;
+    private FirebaseAuth firebaseAuth;
+    private ProgressBar progressBar; //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        editTextFullName = findViewById(R.id.enter_full_name);
+        editTextEmail = findViewById(R.id.enter_email);
+        editTextPassword = findViewById(R.id.enter_password);
+        buttonRegister = findViewById(R.id.register_button);
+        buttonLogin = findViewById(R.id.login_button);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progress_bar);
+
+        /*buttonRegister.setOnClickListener(View.OnClickListener
+        ));*/
+
+    }
+
+    public void onLoginClick(View view) {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+        finish();
     }
 }
